b10e3b1403ca20d860874d1667d0cb3f11a62634
Kirill Varlamov <kirill@ongrid.pro>
2021-07-05 18:33:40 +0300

    Multi-solc hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,27 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("@nomiclabs/hardhat-waffle");
 module.exports = {
-  solidity: "0.8.0",
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.11',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        }
+      },
+      {
+        version: '0.8.0',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        }
+      },
+    ],
+  },
 };
