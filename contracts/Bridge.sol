a2c52f80d0c83a2fb44bc7c4b51fa209e084d270
hideto13 <pinki_00@mail.ru>
2021-10-15 12:16:50 +0300

    Add withdraw in Bridge contract

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2,28 +2,33 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Bridge {
+    using SafeMath for uint256;
+    address public trustedDepositTokenAddress;
 
-    address public trustedDepositTokenAddress; 
+    mapping(address => uint256) public depositedTokens;
 
     constructor(address _trustedDepositTokenAddress) {
         trustedDepositTokenAddress = _trustedDepositTokenAddress;
     }
 
     function deposit(uint256 amountToDeposit) external {
-
         require(amountToDeposit > 0, "Cannot deposit 0 Tokens");
 
-        require(
-            IERC20(trustedDepositTokenAddress).transferFrom(
-                msg.sender,
-                address(this),
-                amountToDeposit
-            ),
-            "Insufficient Token Allowance"
-        );
+        require(IERC20(trustedDepositTokenAddress).transferFrom(msg.sender, address(this), amountToDeposit), "Insufficient Token Allowance");
+
+        depositedTokens[msg.sender] = depositedTokens[msg.sender].add(amountToDeposit);
     }
 
+    function withdraw(uint256 amountToWithdraw) external {
+        require(amountToWithdraw > 0, "Cannot withdraw 0 Tokens!");
+
+        require(depositedTokens[msg.sender] >= amountToWithdraw, "Invalid amount to withdraw");
+
+        require(IERC20(trustedDepositTokenAddress).transfer(msg.sender, amountToWithdraw), "Could not transfer tokens.");
+
+        depositedTokens[msg.sender] = depositedTokens[msg.sender].sub(amountToWithdraw);
+    }
 }
--- test/Bridge.test.js
+++ test/Bridge.test.js
@@ -1,59 +1,114 @@
-const { expect } = require("chai");
-const { parseEther } = ethers.utils;
+const { expect } = require("chai")
+const { parseEther } = ethers.utils
 
 describe("Bridge", function () {
+  beforeEach(async function () {
+    this.signers = await ethers.getSigners()
+    this.owner = this.signers[0]
+    this.account1 = this.signers[1]
 
-    beforeEach(async function () {
-        this.signers = await ethers.getSigners()
-        this.owner = this.signers[0]
-        this.account1 = this.signers[1]
+    this.tokenFactory = await ethers.getContractFactory("DAO1")
+    this.depositToken = await this.tokenFactory.deploy("DAO1", "DAO1", this.owner.address)
+    this.contractFactory = await ethers.getContractFactory("Bridge")
+    this.contract = await this.contractFactory.deploy(this.depositToken.address)
+  })
 
+  it("should be deployed", async function () {
+    expect(await this.contract.deployed(), true)
+    expect(await this.depositToken.deployed(), true)
+  })
 
-        this.tokenFactory = await ethers.getContractFactory("DAO1")
-        this.depositToken = await this.tokenFactory.deploy("DAO1", "DAO1", this.owner.address)
-        this.contractFactory = await ethers.getContractFactory("Bridge")
-        this.contract = await this.contractFactory.deploy(this.depositToken.address)
-      })
+  it("should have correct state variables", async function () {
+    expect(await this.depositToken.owner()).to.equal(this.owner.address)
+  })
 
-
-    it("should be deployed", async function () {
-        expect(await this.contract.deployed(), true)
-        expect(await this.depositToken.deployed(), true)
-    });
-
-    it("should have correct state variables", async function () {
-        expect(await this.depositToken.owner()).to.equal(this.owner.address)
-    })
-
+  describe("Check deposit function", function () {
     it("impossible to deposit zero tokens", async function () {
-        expect(this.contract.deposit(0)).to.be.revertedWith("Cannot deposit 0 Tokens")
+      expect(this.contract.deposit(0)).to.be.revertedWith("Cannot deposit 0 Tokens")
     })
 
     it("impossible to deposit without allowance", async function () {
-        await expect(this.contract.deposit(1)).to.be.revertedWith("ERC20: transfer amount exceeds allowance")
-    })
-
-    it("possible to deposit with allowance", async function () {
-        await this.depositToken.approve(this.contract.address, parseEther("1200000"))
-        expect(await this.contract.deposit(parseEther("500000")), true)
-        let depositBalance = await this.depositToken.balanceOf(this.contract.address)
-        expect(depositBalance).to.equal(parseEther("500000"))
-        let ownerBalance = await this.depositToken.balanceOf(this.owner.address)
-        expect(ownerBalance).to.equal(parseEther("3100000"))
-        expect(await this.contract.deposit(parseEther("700000")), true)
-        ownerBalance = await this.depositToken.balanceOf(this.owner.address)
-        expect(ownerBalance).to.equal(parseEther("2400000"))
-        depositBalance = await this.depositToken.balanceOf(this.contract.address)
-        expect(depositBalance).to.equal(parseEther("1200000"))
+      await expect(this.contract.deposit(1)).to.be.revertedWith("ERC20: transfer amount exceeds allowance")
     })
 
     it("impossible to deposit more than allowance", async function () {
-        await this.depositToken.approve(this.contract.address, "12")
-        await expect(this.contract.deposit(15)).to.be.revertedWith("ERC20: transfer amount exceeds allowance")
+      await this.depositToken.approve(this.contract.address, "12")
+      await expect(this.contract.deposit(15)).to.be.revertedWith("ERC20: transfer amount exceeds allowance")
     })
 
     it("impossible to deposit more than available balance", async function () {
-        await this.depositToken.connect(this.account1).approve(this.contract.address, "12")
-        await expect(this.contract.connect(this.account1).deposit(5)).to.be.revertedWith("ERC20: transfer amount exceeds balance")
+      const depositBalanceAccount = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalanceAccount).to.equal(0)
+      await this.depositToken.connect(this.account1).approve(this.contract.address, "12")
+      await expect(this.contract.connect(this.account1).deposit(5)).to.be.revertedWith("ERC20: transfer amount exceeds balance")
     })
-  });
+
+    it("possible to deposit with allowance", async function () {
+      await this.depositToken.approve(this.contract.address, parseEther("1200000"))
+      expect(await this.contract.deposit(parseEther("1200000")), true)
+      const depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("1200000"))
+      const depositBalanceOwner = await this.contract.depositedTokens(this.owner.address)
+      expect(depositBalanceOwner).to.equal(parseEther("1200000"))
+    })
+
+    it("possible to deposit with allowance in few transactions", async function () {
+      await this.depositToken.approve(this.contract.address, parseEther("1200000"))
+      expect(await this.contract.deposit(parseEther("500000")), true)
+      expect(await this.contract.deposit(parseEther("700000")), true)
+      const depositBalanceOwner = await this.contract.depositedTokens(this.owner.address)
+      expect(depositBalanceOwner).to.equal(parseEther("1200000"))
+      const depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("1200000"))
+    })
+  })
+
+  describe("Check withraw function", function () {
+    it("impossible to withdraw 0 tokens", async function () {
+      await expect(this.contract.withdraw(0)).to.be.revertedWith("Cannot withdraw 0 Tokens")
+    })
+
+    it("impossible to withdraw more than balance", async function () {
+      const deposit = await this.contract.depositedTokens(this.owner.address)
+      expect(deposit).to.equal(0)
+      await expect(this.contract.withdraw(1)).to.be.revertedWith("Invalid amount to withdraw")
+    })
+
+    it("impossible to withdraw another sender's tokens", async function () {
+      await this.depositToken.approve(this.contract.address, parseEther("500000"))
+      expect(await this.contract.deposit(parseEther("500000")), true)
+      const depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("500000"))
+      const depositBalanceOwner = await this.contract.depositedTokens(this.owner.address)
+      expect(depositBalanceOwner).to.equal(parseEther("500000"))
+      const depositBalanceAccount = await this.contract.depositedTokens(this.account1.address)
+      expect(depositBalanceAccount).to.equal(0)
+      await expect(this.contract.connect(this.account1).withdraw(parseEther("300000"))).to.be.revertedWith("Invalid amount to withdraw")
+    })
+
+    it("possible to withdraw after deposit", async function () {
+      await this.depositToken.approve(this.contract.address, parseEther("500000"))
+      expect(await this.contract.deposit(parseEther("500000")), true)
+      expect(await this.contract.withdraw(parseEther("300000")), true)
+      let depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("200000"))
+    })
+
+    it("correct balances after withdraw", async function () {
+      await this.depositToken.transfer(this.account1.address, parseEther("500000"))
+      await this.depositToken.approve(this.contract.address, parseEther("500000"))
+      await this.depositToken.connect(this.account1).approve(this.contract.address, parseEther("500000"))
+      expect(await this.contract.deposit(parseEther("500000")), true)
+      expect(await this.contract.connect(this.account1).deposit(parseEther("500000")), true)
+      let depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("1000000"))
+      expect(await this.contract.connect(this.account1).withdraw(parseEther("300000")), true)
+      let depositBalanceOwner = await this.contract.depositedTokens(this.owner.address)
+      expect(depositBalanceOwner).to.equal(parseEther("500000"))
+      let depositBalanceAccount = await this.contract.depositedTokens(this.account1.address)
+      expect(depositBalanceAccount).to.equal(parseEther("200000"))
+      depositBalance = await this.depositToken.balanceOf(this.contract.address)
+      expect(depositBalance).to.equal(parseEther("700000"))
+    })
+  })
+})
