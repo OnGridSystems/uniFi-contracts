9c3f41629eebab5281e120479fa3fea58ac75533
Dmitry Suldin <d.suldin@ongrid.pro>
2021-10-29 09:39:13 +0300

    Add test for call function and public variable

--- test/UniFiDaiVault.test.js
+++ test/UniFiDaiVault.test.js
@@ -1,7 +1,5 @@
-//const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { expect } = require("chai")
 const { BigNumber } = require("ethers")
-//const { ZERO_ADDRESS } = constants;
 
 describe("UniFiStake", function () {
   before(async function () {
@@ -39,16 +37,19 @@ describe("UniFiStake", function () {
     expect(await this.pool.owner()).to.equal(this.owner.address)
   })
 
-  it("function claim()", async function () {
-    
-    balance=1000
-    await this.rewardToken.approve(this.pool.address, balance)
-    await this.pool.addContractBalance(balance)
+  it("function getHoldersList()", async function () {
+    getHoldersList_after = await this.pool.getHoldersList(0, 1)
+    block = await network.provider.send("eth_getBlockByNumber", ["latest", false])
 
-    await this.pool.connect(this.alice).claim()
-    alice_reward_balance = await this.rewardToken.balanceOf(this.alice.address)
+    stakers = getHoldersList_after["stakers"][0]
+    stakingTimestamps = getHoldersList_after["stakingTimestamps"][0]
+    lastClaimedTimeStamps = getHoldersList_after["lastClaimedTimeStamps"][0]
+    stakedTokens = getHoldersList_after["stakedTokens"][0]
 
-    expect(balance-1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
+    expect(stakers).to.equal(this.alice.address)
+    expect(stakingTimestamps).to.equal(block.timestamp)
+    expect(lastClaimedTimeStamps).to.equal(block.timestamp)
+    expect(stakedTokens).to.equal(this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000")))
   })
 
   it("reward logic // logs above", async function () {
@@ -56,16 +57,16 @@ describe("UniFiStake", function () {
     threeSign = this.signers[4]
     fourSign = this.signers[5]
 
-    two_deposit=this.alice_deposit.mul('6')
-    three_deposit=this.alice_deposit.mul('25').div('10')
-    four_deposit=this.alice_deposit.mul('5').div('10')
+    two_deposit = this.alice_deposit.mul("6")
+    three_deposit = this.alice_deposit.mul("25").div("10")
+    four_deposit = this.alice_deposit.mul("5").div("10")
 
     await this.depositToken.connect(this.bob).transfer(twoSign.address, two_deposit)
     await this.depositToken.connect(this.bob).transfer(threeSign.address, three_deposit)
     await this.depositToken.connect(this.bob).transfer(fourSign.address, four_deposit)
 
     await this.depositToken.connect(twoSign).approve(this.pool.address, two_deposit)
-    await this.depositToken.connect(threeSign).approve(this.pool.address,three_deposit)
+    await this.depositToken.connect(threeSign).approve(this.pool.address, three_deposit)
     await this.depositToken.connect(fourSign).approve(this.pool.address, four_deposit)
 
     await this.pool.connect(twoSign).deposit(two_deposit)
@@ -74,7 +75,7 @@ describe("UniFiStake", function () {
 
     total_tokens = two_deposit.add(three_deposit).add(four_deposit).add(this.alice_deposit)
     console.log(total_tokens.toString())
-    total_reward=BigNumber.from("100")
+    total_reward = BigNumber.from("100")
     await this.rewardToken.approve(this.pool.address, total_reward)
     await this.pool.addContractBalance(total_reward)
 
@@ -84,22 +85,59 @@ describe("UniFiStake", function () {
     await this.pool.connect(fourSign).claim()
 
     alice_reward = await this.rewardToken.balanceOf(this.alice.address)
-    two_reward=await this.rewardToken.balanceOf(twoSign.address)
-    three_reward=await this.rewardToken.balanceOf(threeSign.address)
-    four_reward=await this.rewardToken.balanceOf(fourSign.address)
-    
+    two_reward = await this.rewardToken.balanceOf(twoSign.address)
+    three_reward = await this.rewardToken.balanceOf(threeSign.address)
+    four_reward = await this.rewardToken.balanceOf(fourSign.address)
+
     console.log("total number of reward tokens", total_reward.toString())
-    console.log("total number of award tokens issued",(two_reward.add(three_reward).add(four_reward).add(alice_reward)).toString())
-    console.log("one_reward", two_reward.toString() )
-    console.log("two_reward", three_reward.toString() )
+    console.log("total number of award tokens issued", two_reward.add(three_reward).add(four_reward).add(alice_reward).toString())
+    console.log("one_reward", two_reward.toString())
+    console.log("two_reward", three_reward.toString())
     console.log("alice_reward", alice_reward.toString())
     console.log("three_reward", four_reward.toString())
-    console.log("approximately the percentage of the deposit from the total number of invested tokens\nis the percentage of reward tokens that the user will receive")
+    console.log(
+      "approximately the percentage of the deposit from the total number of invested tokens\nis the percentage of reward tokens that the user will receive"
+    )
 
-    expect((this.alice_deposit.mul(total_reward).div(total_tokens)).sub('1')).to.equal(alice_reward) // 1 is lost during calculations reward
-    expect((two_deposit.mul(total_reward).div(total_tokens)).sub('1')).to.equal(two_reward) // 1 is lost during calculations reward
-    expect((three_deposit.mul(total_reward).div(total_tokens)).sub('1')).to.equal(three_reward) // 1 is lost during calculations reward
-    expect((four_deposit.mul(total_reward).div(total_tokens)).sub('1')).to.equal(four_reward) // 1 is lost during calculations reward
+    expect(this.alice_deposit.mul(total_reward).div(total_tokens).sub("1")).to.equal(alice_reward) // 1 is lost during calculations reward
+    expect(two_deposit.mul(total_reward).div(total_tokens).sub("1")).to.equal(two_reward) // 1 is lost during calculations reward
+    expect(three_deposit.mul(total_reward).div(total_tokens).sub("1")).to.equal(three_reward) // 1 is lost during calculations reward
+    expect(four_deposit.mul(total_reward).div(total_tokens).sub("1")).to.equal(four_reward) // 1 is lost during calculations reward
+  })
+
+  describe("function claim()", function () {
+    it("transfers reward tokens to the user", async function () {
+      balance = 1000
+      await this.rewardToken.approve(this.pool.address, balance)
+      await this.pool.addContractBalance(balance)
+
+      await this.pool.connect(this.alice).claim()
+      alice_reward_balance = await this.rewardToken.balanceOf(this.alice.address)
+
+      expect(balance - 1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
+    })
+
+    it("increase totalEarnedTokens when receiving reward funds", async function () {
+      total_before_claim = await this.pool.totalEarnedTokens(this.alice.address)
+      expect(0).to.equal(total_before_claim)
+
+      balance = 1000
+      await this.rewardToken.approve(this.pool.address, balance)
+      await this.pool.addContractBalance(balance)
+
+      await this.pool.connect(this.alice).claim()
+      alice_reward_balance = await this.rewardToken.balanceOf(this.alice.address)
+      total_after_claim = await this.pool.totalEarnedTokens(this.alice.address)
+
+      expect(total_after_claim).to.equal(alice_reward_balance)
+    })
+
+    it("increasing the lastClaimedTime when claim", async function () {
+      await this.pool.connect(this.alice).claim()
+      lastClaimedTime = await this.pool.lastClaimedTime(this.alice.address)
+      block = await network.provider.send("eth_getBlockByNumber", ["latest", false])
+      expect(lastClaimedTime).to.equal(block.timestamp)
+    })
   })
 
   describe("function deposit()", function () {
@@ -118,8 +156,8 @@ describe("UniFiStake", function () {
     it("creating a position for the user", async function () {
       deposit = await this.pool.depositedTokens(this.alice.address)
       depositTime = await this.pool.depositTime(this.alice.address)
-      blockTime = await network.provider.send("eth_getBlockByNumber", ["latest", false])
-      expect(blockTime.timestamp).to.equal(depositTime._hex)
+      block = await network.provider.send("eth_getBlockByNumber", ["latest", false])
+      expect(block.timestamp).to.equal(depositTime._hex)
       expected_deposit = this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000"))
       expect(deposit).to.equal(expected_deposit)
     })
@@ -129,20 +167,101 @@ describe("UniFiStake", function () {
       balance = this.alice_deposit.mul("50").div("10000")
       expect(ownerBalance).to.equal(balance)
     })
+    describe("second holder deposit", function () {
+      beforeEach(async function () {
+        getNumberOfHolders_before = await this.pool.getNumberOfHolders()
+        await this.depositToken.connect(this.bob).approve(this.pool.address, this.alice_deposit)
+        await this.pool.connect(this.bob).deposit(this.alice_deposit)
+        getNumberOfHolders_after = await this.pool.getNumberOfHolders()
+      })
+
+      describe("increasing getNumberOfHolders(), getHoldersList() on deposit ", function () {
+        it("increasing getNumberOfHolders when deposit", async function () {
+          expect(getNumberOfHolders_before.add(1)).to.equal(getNumberOfHolders_after)
+        })
+
+        it("increasing getHoldersList when deposit", async function () {
+          getHoldersList_after = await this.pool.getHoldersList(0, getNumberOfHolders_after)
+          stakers_length = getHoldersList_after["stakers"].length
+          stakingTimestamps_length = getHoldersList_after["stakingTimestamps"].length
+          lastClaimedTimeStamps_length = getHoldersList_after["lastClaimedTimeStamps"].length
+          stakedTokens_length = getHoldersList_after["stakedTokens"].length
+          expect(stakers_length).to.equal(getNumberOfHolders_after)
+          expect(stakingTimestamps_length).to.equal(getNumberOfHolders_after)
+          expect(lastClaimedTimeStamps_length).to.equal(getNumberOfHolders_after)
+          expect(stakedTokens_length).to.equal(getNumberOfHolders_after)
+        })
+      })
+
+      describe("decreasing getNumberOfHolders(), getHoldersList() on withdraw ", function () {
+        beforeEach(async function () {
+          LOCKUP_TIME = await this.pool.LOCKUP_TIME()
+          await network.provider.send("evm_increaseTime", [parseInt(LOCKUP_TIME) + 1])
+        })
+        describe("func withdraw()", function () {
+          it("decreasing getNumberOfHolders when withdraw all holder deposit", async function () {
+            getNumberOfHolders_before = await this.pool.getNumberOfHolders()
+            await this.pool.connect(this.alice).withdraw(this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000")))
+            getNumberOfHolders_after = await this.pool.getNumberOfHolders()
+            expect(getNumberOfHolders_before.sub("1")).to.equal(getNumberOfHolders_after)
+          })
+
+          it("decreasing getHoldersList when withdraw", async function () {
+            await this.pool.connect(this.alice).withdraw(this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000")))
+            getHoldersList_after = await this.pool.getHoldersList(0, getNumberOfHolders_after.sub("1"))
+
+            stakers_length = getHoldersList_after["stakers"].length
+            stakingTimestamps_length = getHoldersList_after["stakingTimestamps"].length
+            lastClaimedTimeStamps_length = getHoldersList_after["lastClaimedTimeStamps"].length
+            stakedTokens_length = getHoldersList_after["stakedTokens"].length
+            expected_length = getNumberOfHolders_after.sub("1")
+
+            expect(stakers_length).to.equal(expected_length)
+            expect(stakingTimestamps_length).to.equal(expected_length)
+            expect(lastClaimedTimeStamps_length).to.equal(expected_length)
+            expect(stakedTokens_length).to.equal(expected_length)
+          })
+        })
+
+        describe("func emergencyWithdraw()", function () {
+          it("decreasing getNumberOfHolders when emergencyWithdraw all holder deposit", async function () {
+            lastClaimedTime_before = await this.pool.getNumberOfHolders()
+            await this.pool.connect(this.alice).emergencyWithdraw(this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000")))
+            lastClaimedTime_after = await this.pool.getNumberOfHolders()
+            expect(lastClaimedTime_before.sub("1")).to.equal(lastClaimedTime_after)
+          })
+
+          it("decreasing getHoldersList when emergencyWithdraw", async function () {
+            await this.pool.connect(this.alice).emergencyWithdraw(this.alice_deposit.sub(this.alice_deposit.mul("50").div("10000")))
+            getHoldersList_after = await this.pool.getHoldersList(0, getNumberOfHolders_after.sub("1"))
+
+            stakers_length = getHoldersList_after["stakers"].length
+            stakingTimestamps_length = getHoldersList_after["stakingTimestamps"].length
+            lastClaimedTimeStamps_length = getHoldersList_after["lastClaimedTimeStamps"].length
+            stakedTokens_length = getHoldersList_after["stakedTokens"].length
+            expected_length = getNumberOfHolders_after.sub("1")
+
+            expect(stakers_length).to.equal(expected_length)
+            expect(stakingTimestamps_length).to.equal(expected_length)
+            expect(lastClaimedTimeStamps_length).to.equal(expected_length)
+            expect(stakedTokens_length).to.equal(expected_length)
+          })
+        })
+      })
+    })
 
     it("getting a reward for not the first deposits", async function () {
       await this.depositToken.connect(this.bob).transfer(this.alice.address, this.alice_deposit)
       await this.depositToken.connect(this.alice).approve(this.pool.address, this.alice_deposit)
 
-      balance=1000
+      balance = 1000
       await this.rewardToken.approve(this.pool.address, balance)
       await this.pool.addContractBalance(balance)
 
       await this.pool.connect(this.alice).deposit(this.alice_deposit)
       alice_reward_balance = await this.rewardToken.balanceOf(this.alice.address)
 
-      expect(balance-1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
-
+      expect(balance - 1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
     })
 
     it("can't deposit 0 token", async function () {
@@ -214,16 +333,14 @@ describe("UniFiStake", function () {
       })
 
       it("getting a reward when withdrawing funds", async function () {
-        
-        balance=1000
+        balance = 1000
         await this.rewardToken.approve(this.pool.address, balance)
         await this.pool.addContractBalance(balance)
-
 
         await this.pool.connect(this.alice).withdraw(this.half_alice_deposit)
         alice_reward_balance = await this.rewardToken.balanceOf(this.alice.address)
 
-        expect(balance-1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
+        expect(balance - 1).to.equal(alice_reward_balance) // 1 is lost during calculations reward
       })
     })
   })
@@ -280,6 +397,13 @@ describe("UniFiStake", function () {
         contract_balance = await this.pool.totalTokens()
         expected_balance = this.alice_deposit.sub(this.half_alice_deposit).sub(this.alice_deposit.mul("50").div("10000"))
         expect(expected_balance).to.equal(contract_balance)
+      })
+
+      it("increasing the lastClaimedTime when emergencyWithdraw", async function () {
+        await this.pool.connect(this.alice).emergencyWithdraw(this.half_alice_deposit)
+        lastClaimedTime = await this.pool.lastClaimedTime(this.alice.address)
+        block = await network.provider.send("eth_getBlockByNumber", ["latest", false])
+        expect(lastClaimedTime).to.equal(block.timestamp)
       })
 
       it("emergency withdrawing does not issue a reward", async function () {
@@ -390,17 +514,25 @@ describe("UniFiStake", function () {
   })
 
   describe("function addContractBalance()", async function () {
+    beforeEach(async function () {
+      await this.rewardToken.approve(this.pool.address, 1000)
+      await this.pool.addContractBalance(1000)
+      balance = await this.rewardToken.balanceOf(this.pool.address)
+    })
+
     it("function only owner", async function () {
       await expect(this.pool.connect(this.alice).addContractBalance(1000)).to.be.revertedWith("Ownable: caller is not the owner")
     })
 
     it("increase contractBalance", async function () {
-      await this.rewardToken.approve(this.pool.address, 1000)
-      await this.pool.addContractBalance(1000)
-      balance = await this.rewardToken.balanceOf(this.pool.address)
       contractBalance = await this.pool.contractBalance()
       expect(balance).to.equal(1000)
       expect(contractBalance).to.equal(1000)
+    })
+
+    it("increase getEstimatedPendingDivs", async function () {
+      getEstimatedPendingDivs = await this.pool.getEstimatedPendingDivs(this.alice.address)
+      expect(getEstimatedPendingDivs).to.equal(balance)
     })
   })
 })
