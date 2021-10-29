b8a60c87d15904c04448523307b6618da5897602
Dmitry Suldin <d.suldin@ongrid.pro>
2021-10-29 09:39:13 +0300

    Add basic test for L2Bridge

---
+++ test/L2Bridge.test.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai")
+const { parseEther } = ethers.utils
+
+describe("L2Bridge", function () {
+  beforeEach(async function () {
+    this.signers = await ethers.getSigners()
+    this.owner = this.signers[0]
+    this.account1 = this.signers[1]
+
+    this.dao1TokenFactory = await ethers.getContractFactory("DAO1")
+    this.dao1Token = await this.dao1TokenFactory.deploy("DAO1", "DAO1", this.owner.address)
+    this.bridgeFactory = await ethers.getContractFactory("L2Bridge")
+    this.bridge = await this.bridgeFactory.deploy(this.dao1Token.address)
+  })
+
+  it("should be deployed", async function () {
+    expect(await this.bridge.deployed(), true)
+    expect(await this.dao1Token.deployed(), true)
+  })
+
+  it("outboundTransfer: unable to send 0 tokens", async function () {
+    expect(this.bridge.outboundTransfer(0)).to.be.revertedWith("Cannot burn 0 Tokens")
+  })
+
+  it("finalizeInboundTransfer: unable to mint 0 tokens", async function () {
+    expect(
+      this.bridge.finalizeInboundTransfer(this.owner.address, 0, "0x117ddadadc7b8d342cf48513fef06a2cba15dfb9c488dc51aefc998abcefb52b")
+    ).to.be.revertedWith("Cannot mint 0 Tokens")
+  })
+})
